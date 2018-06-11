3df78186ea585040fc3b09751d185181dac1dde5
synergy2411 <synergy2411@outlook.com>
2018-06-11 15:32:30 +0530

    bootstrap added

---
+++ src/app/user/user.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector : 'app-user',
+    //template : `<h1>User Component Loaded!!!</h1>`
+    templateUrl : './user.component.html'
+})
+export class UserComponent{
+    firstName : string = "Bill";
+    user = {
+        firstName : "Bill",
+        lastName : "Gates",
+        company : "Microsoft",
+        income : 50000,
+        dob : new Date("Dec 21, 1965"),
+        isWorking : true
+    }
+}
